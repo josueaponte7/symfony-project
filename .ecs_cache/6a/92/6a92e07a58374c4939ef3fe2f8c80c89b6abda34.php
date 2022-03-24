779c6cc7a8d4bb3770d0cdd2851af6b4d9afd035
Josue Aponte <josueaponte7@gmail.com>
2022-03-24 21:15:28 +0100

    Clean Code

---
+++ .ecs_cache/6a/92/6a92e07a58374c4939ef3fe2f8c80c89b6abda34.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return Symplify\EasyCodingStandard\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'file_hash',
+   'data' => 'fcbf352254a65ed5c74131183b2e4db991ee367f57b11018e004f5a595bc2be8',
+));
